ba0944fa84a46303fb9c66bba93d8d5c00058407
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-01-18 22:57:57 +0300

    added util functions

--- xstrings.go
+++ xstrings.go
@@ -1,1 +1,46 @@
 package xstrings
+
+import (
+	"strconv"
+	"unicode"
+)
+
+func TryUnquote(s string) string {
+	str, err := strconv.Unquote(s)
+	if err != nil {
+		return s
+	}
+	return str
+}
+
+func ToUpperBeginning(str string) string {
+	if str == "" {
+		return ""
+	}
+	buf := make([]byte, 0, len(str))
+	var idx int
+	for _, r := range str {
+		if idx <= 0 {
+			r = unicode.ToUpper(r)
+		}
+		buf = append(buf, []byte(string(r))...)
+		idx++
+	}
+	return string(buf)
+}
+
+func ToLowerBeginning(str string) string {
+	if str == "" {
+		return ""
+	}
+	buf := make([]byte, 0, len(str))
+	var idx int
+	for _, r := range str {
+		if idx <= 0 {
+			r = unicode.ToLower(r)
+		}
+		buf = append(buf, []byte(string(r))...)
+		idx++
+	}
+	return string(buf)
+}
