c71bad9851f005b8b158a22d11604f559a3b4905
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-01-18 22:48:14 +0300

    reserved example1

--- examples/example1/main.go
+++ examples/example1/main.go
@@ -3,62 +3,13 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
-
 	"github.com/goinsane/xstrings"
 )
 
 func main() {
-	p := xstrings.Unmarshaler{}
+	u := xstrings.NewUnmarshaler()
+	m := xstrings.NewMarshaler()
 
-	str := "10"
-
-	var x int
-	err := p.UnmarshalByValue(str, reflect.ValueOf(&x))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(x)
-
-	var y int
-	err = p.Unmarshal(str, &y)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(y)
-
-	z := 10
-	ifc, err := p.Parse("25", reflect.TypeOf(z))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	if ifc == nil {
-		fmt.Println("nil1")
-		return
-	}
-	fmt.Println(ifc.(int))
-
-	var s struct {
-		X int
-		Y int
-	}
-	err = p.Unmarshal(`{"X": 5, "Y": 6}`, &s)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(s)
-
-	var a *int
-	//a = new(int)
-	err = p.Unmarshal(`10`, &a)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(*a)
+	_ = u
+	_ = m
 }
